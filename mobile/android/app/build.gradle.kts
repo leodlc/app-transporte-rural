c5d9d2d9434d4c1933b0d9f388913ad80aea81fb
sddrouet <sddrouet@espe.edu.ec>
2025-05-19 17:24:06 -0500

    fix dependencies on android

--- mobile/android/app/build.gradle.kts
+++ mobile/android/app/build.gradle.kts
@@ -8,14 +8,17 @@ plugins {
     id("dev.flutter.flutter-gradle-plugin")
 }
 
+
 android {
     namespace = "com.example.mobile"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
+
+        isCoreLibraryDesugaringEnabled = true
     }
 
     kotlinOptions {
@@ -40,6 +43,10 @@ android {
             signingConfig = signingConfigs.getByName("debug")
         }
     }
+}
+
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.5")
 }
 
 flutter {
--- mobile/lib/config/api_config.dart
+++ mobile/lib/config/api_config.dart
@@ -1,4 +1,4 @@
 class ApiConfig {
   static const String baseUrl =
-      "http://192.168.100.179:3000"; // Cambiar aquí cuando se suba a la nube
+      "http://localhost:3000"; // Cambiar aquí cuando se suba a la nube
 }
